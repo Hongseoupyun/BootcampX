d3fb9cc4a102bdb464f967cb78bb6be5551f9d89
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-05-04 23:49:46 -0400

    Protech sql injections; parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`SELECT DISTINCT teachers.name as teacher , cohorts.name as cohort
+const queryString = `
+SELECT DISTINCT teachers.name as teacher , cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]|| 'JUL02'}'
-ORDER BY teachers.name;`
-).then(res=>{
-  console.log('connected')
-  res.rows.forEach(users=>{console.log(`${users.cohort}`+ ':' + `${users.teacher}`)
+WHERE cohorts.name = $1
+ORDER BY teachers.name;
+  `;
 
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+
+pool.query(queryString, values).then(res => {
+  console.log('connected')
+  res.rows.forEach(users => {
+    console.log(`${users.cohort}` + ':' + `${users.teacher}`)
   })
-  
+
 }).catch(err => console.error('query error', err.stack));
